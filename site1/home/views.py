411837c6794a8167758048255885189f44df0e74
Dungdao2309 <dungdv1514@ut.edu.vn>
2024-12-15 21:56:43 +0700

    Delete site1 directory

--- site1/home/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-def get_home(request):
-<<<<<<< HEAD
-    return render(request, 'home.html')
-=======
-    return render(request,'home.html')
->>>>>>> 1e2ef9fc9e3d3539620c61f682e9391d64602688
